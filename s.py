0ceb3ee22c7c477aab027228b8ba41a4ace914ff
megh2506 <meghanaadrushappanavar@gmail.com>
2025-11-15 08:58:51 +0530

    Create s.py

---
+++ s.py
@@ -0,0 +1,7 @@
+try:
+  a=float(input("enter first number:"))
+  b=float(input("enter second number:"))
+  print(f"addition:{a+b}")
+  print(f"subtraction:{a-b}")
+exceptValueError:
+  print("please enter valid numbers:")
